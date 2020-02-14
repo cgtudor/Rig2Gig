e7e4ca4f7b6f359866acb7f6c43f1b073913964e
bsouch96 <v8178742@live.tees.ac.uk>
2020-02-14 22:43:02 +0000

    Removed unused imports

--- app/src/main/java/com/gangoffive/rig2gig/MainActivity.java
+++ app/src/main/java/com/gangoffive/rig2gig/MainActivity.java
@@ -1,20 +1,13 @@
   package com.gangoffive.rig2gig;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
 
   public class MainActivity extends AppCompatActivity {
 
