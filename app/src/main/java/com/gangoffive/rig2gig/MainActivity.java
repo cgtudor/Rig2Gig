e9e402a5999dd7f81b3d573715129994436e655a
Carter Thomas RIDGEWAY <v8265314@linux17.scm.tees.ac.uk>
2020-02-14 11:43:34 +0000

    Revert "Added authentication for log in a register"
    This reverts commit e08c5ef69fd2d9394ae26acea116b38f15008622.

--- app/src/main/java/com/gangoffive/rig2gig/MainActivity.java
+++ app/src/main/java/com/gangoffive/rig2gig/MainActivity.java
@@ -2,36 +2,13 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-  public class MainActivity extends AppCompatActivity {
-
-    Button logOutBtn;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        logOutBtn = findViewById(R.id.logOutButton);
-
-        /**
-         *  Logging Out Current User
-         */
-        logOutBtn.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-                finish();
-            }
-        });
     }
 }
