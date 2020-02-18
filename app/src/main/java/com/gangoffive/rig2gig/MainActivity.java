8a1e51c66448297bd2f86223a7d32210e1112f2d
J-Jardine <t7077222@live.tees.ac.uk>
2020-02-18 16:50:48 +0000

    Added modified files to commit

--- app/src/main/java/com/gangoffive/rig2gig/MainActivity.java
+++ app/src/main/java/com/gangoffive/rig2gig/MainActivity.java
@@ -1,53 +1,27 @@
   package com.gangoffive.rig2gig;
 
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import org.w3c.dom.Document;
-
-import java.util.HashMap;
-import java.util.Map;
+import android.view.View;
 
   public class MainActivity extends AppCompatActivity {
-
-      FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Db test
-        fStore = FirebaseFirestore.getInstance();
-
-        DocumentReference documentReference = fStore.collection("Testing").document("testId");
-        Map<String,Object> testInput = new HashMap<>();
-        testInput.put("firstName", "Jacob");
-        testInput.put("lastName", "Jardine");
-        documentReference.set(testInput).addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                System.out.println("------------------------Worked");
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                System.out.println("------------------------Didn't Work");
-            }
-        });
-
-        //Following intent creates the navbar activity. Ensure last in executions.
-        Intent intent = new Intent(this, NavBarActivity.class);
-        startActivity(intent);
     }
-}
+
+    public void loginBtnOnClick(View view) {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+
+    public void registerBtnOnClick(View view) {
+        startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+    }
+
+
+  }
--- app/src/main/java/com/gangoffive/rig2gig/RegisterActivity.java
+++ app/src/main/java/com/gangoffive/rig2gig/RegisterActivity.java
@@ -1,14 +1,90 @@
 package com.gangoffive.rig2gig;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+    public static final String TAG = "TAG";
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
+    EditText rFirstName, rLastName, rEmailAddress, rConfirmEmail, rPassword, rConfirmPassword;
+    Button rRegisterBtn;
+
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        rFirstName = findViewById(R.id.registerFirstName);
+        rLastName = findViewById(R.id.registerLastName);
+        rEmailAddress = findViewById(R.id.registerEmail);
+        rConfirmEmail = findViewById(R.id.registerConfirmEmail);
+        rPassword = findViewById(R.id.registerPassword);
+        rConfirmPassword = findViewById(R.id.registerConfirmPassword);
+        rRegisterBtn = findViewById(R.id.registerBtn);
+    }
+
+    public void registerBtnOnClick(View view) {
+        final String email = rEmailAddress.getText().toString().trim();
+        String password = rPassword.getText().toString().trim();
+        final String fullName = rFirstName.getText().toString() + " " + rLastName.getText().toString();
+
+        //Validation needed to check inputs
+
+        fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    userId = fAuth.getUid();
+                    DocumentReference documentReference = fStore.collection("users").document(userId);
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("Email Address", email);
+                    user.put("Full Name", fullName);
+                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "onSuccess: user Profile is created for "+ userId);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "onFailure: " + e.toString());
+                        }
+                    });
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                }else {
+                    Toast.makeText(RegisterActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
+
