98dbc8cb526f04ec96b97de8379783e8c577d5f6
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2020-02-18 17:02:20 +0000

    Change image implemented

--- app/src/main/java/com/gangoffive/rig2gig/CreatePerformerListing.java
+++ app/src/main/java/com/gangoffive/rig2gig/CreatePerformerListing.java
@@ -1,7 +1,13 @@
 package com.gangoffive.rig2gig;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,9 +19,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,7 +31,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,18 +50,26 @@ import static androidx.constraintlayout.widget.Constraints.TAG;
 public class CreatePerformerListing extends Fragment {
 
     private TextView name, location, genres, charge, distance, description;
-    private Button createListing;
+    private ImageView image;
+    private Button createListing, changeImage;
     private String bandRef;
     private FirebaseFirestore db;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private StorageReference bandImageRef;
     private Map<String, Object> listing;
     private String invalidFields;
-    Map <String, Object> band;
+    private Map <String, Object> band;
+    private static final int ACTION_PERFORMER_LISTING_PHOTO = 100;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bandRef = "TvuDGJwqX13vJ6LWZYB2";
         db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        bandImageRef = storageRef.child("/images/bands/" + bandRef + "/profile.jpg");
         DocumentReference docRef = db.collection("bands").document(bandRef);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -68,8 +93,7 @@ public class CreatePerformerListing extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create_performer_listing, container, false);
         setInputReferences(view);
         createListing.setOnClickListener(new View.OnClickListener() {
@@ -79,19 +103,45 @@ public class CreatePerformerListing extends Fragment {
                 //need to check if listing exists, perhaps by a start and end date
                 Map<String, Object> listing = bandDataMap();
                 Boolean valid = validateDataMap();
-                if (valid)
-                {
+                if (valid) {
                     postDataToDatabase();
-                }
-                else
-                {
+                } else {
                     Toast.makeText(getActivity(), "Listing not created.  The following fields " +
                             "are incomplete:\n" + invalidFields, Toast.LENGTH_LONG).show();
                 }
             }
         });
+        changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, ACTION_PERFORMER_LISTING_PHOTO);
+            }
+        });
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ACTION_PERFORMER_LISTING_PHOTO && resultCode == Activity.RESULT_OK && data != null)
+        {
+            try
+            {
+                InputStream iStream = getContext().getContentResolver().openInputStream(data.getData());
+                image.setImageDrawable(Drawable.createFromStream(iStream, data.getData().toString()));
+            } catch (FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
 
     public void setInputReferences(View view)
     {
@@ -102,6 +152,8 @@ public class CreatePerformerListing extends Fragment {
         distance = view.findViewById(R.id.distance);
         description = view.findViewById(R.id.description);
         createListing = view.findViewById(R.id.createPerformerListing);
+        changeImage = view.findViewById(R.id.changeImage);
+        image = view.findViewById(R.id.image);
     }
 
     public void populateFields()
@@ -113,6 +165,9 @@ public class CreatePerformerListing extends Fragment {
         charge.setText(String.valueOf(band.get("chargePerHour")));
         distance.setText(String.valueOf(band.get("travelDistance")));
         description.setText((String)band.get("description"));
+        GlideApp.with(this)
+                .load(bandImageRef)
+                .into(image);
     }
 
     public Map<String, Object> bandDataMap()
@@ -153,6 +208,43 @@ public class CreatePerformerListing extends Fragment {
                     @Override
                     public void onSuccess(DocumentReference documentReference) {
                         Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                        StorageReference listingImage = storageRef.child("/images/performance-listings/" + documentReference.getId() + ".jpg");
+
+// Get the data from an ImageView as bytes
+                        image.setDrawingCacheEnabled(true);
+                        image.buildDrawingCache();
+                        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+                        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                        byte[] data = baos.toByteArray();
+
+                        UploadTask uploadTask = listingImage.putBytes(data);
+                        uploadTask.addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle unsuccessful uploads
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                                // ...
+                            }
+                        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                         //need to pass to view listing activity to be called "PerformanceListingDetailsActivity"
                         getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyBandFragment()).commit();
                     }
