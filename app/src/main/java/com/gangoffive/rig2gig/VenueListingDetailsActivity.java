905741e7f617ddd838f22c7a687cbdad8507ce7f
Cristian-Gabriel TUDOR <v8002382@linux8.scm.tees.ac.uk>
2020-02-21 21:33:18 +0000

    Feature implemented and working

---
+++ app/src/main/java/com/gangoffive/rig2gig/VenueListingDetailsActivity.java
@@ -0,0 +1,104 @@
+package com.gangoffive.rig2gig;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class VenueListingDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venue_listing_details);
+
+        final ImageView venuePhoto = findViewById(R.id.venuePhoto);
+        final TextView venueName = findViewById(R.id.venueName);
+        final TextView description = findViewById(R.id.description);
+        final TextView rating = findViewById(R.id.rating);
+        final TextView location = findViewById(R.id.location);
+
+        /*Used to get the id of the listing from the previous activity*/
+/*        String vID = getIntent().getStringExtra("EXTRA_VENUE_LISTING_ID");*/
+
+        String vID = "8Qy8Sz8Ab5Q6FVebgxXL";
+
+        /*Firestore & Cloud Storage initialization*/
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+
+        /*Finding the listing by its ID in the "venue-listings" subfolder*/
+        DocumentReference venueListing = db.collection("venue-listings").document(vID);
+
+        /*Retrieving information from the reference, listeners allow use to change what we do in case of success/failure*/
+        venueListing.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("FIRESTORE", "DocumentSnapshot data: " + document.getData());
+
+                        /*Find the venue reference by looking for the venue ID in the "venues" subfolder*/
+                        DocumentReference venue = db.collection("venues").document(document.get("venue-ref").toString());
+
+                        venue.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists()) {
+                                        Log.d("FIRESTORE", "DocumentSnapshot data: " + document.getData());
+
+                                        venueName.setText(document.get("name").toString());
+                                        rating.setText("Rating: " + document.get("rating").toString() + "/5");
+                                        location.setText(document.get("location").toString());
+
+                                    } else {
+                                        Log.d("FIRESTORE", "No such document");
+                                    }
+                                } else {
+                                    Log.d("FIRESTORE", "get failed with ", task.getException());
+                                }
+                            }
+                        });
+                        description.setText(document.get("description").toString());
+                    } else {
+                        Log.d("FIRESTORE", "No such document");
+                    }
+                } else {
+                    Log.d("FIRESTORE", "get failed with ", task.getException());
+                }
+            }
+        });
+
+        //Temp wait for pic to upload
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        /*Find reference for the photo associated with the listing inside the according subtree*/
+        StorageReference venuePic = storage.getReference().child("/images/venue-listings/" + vID + ".jpg");
+
+        /*Using Glide to load the picture from the reference directly into the ImageView*/
+        GlideApp.with(this /* context */)
+                .load(venuePic)
+                .into(venuePhoto);
+    }
+}
