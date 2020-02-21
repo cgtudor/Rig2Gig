866e45c33a569c0b6fbc67785f2db8523bd9ffd3
J-Jardine <t7077222@live.tees.ac.uk>
2020-02-21 16:08:53 +0000

    Setting up branch for development on creating and viewing bands created

---
+++ app/src/main/java/com/gangoffive/rig2gig/CreateBandActivity.java
@@ -0,0 +1,14 @@
+package com.gangoffive.rig2gig;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateBandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_band);
+    }
+}
