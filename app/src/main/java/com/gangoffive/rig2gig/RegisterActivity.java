2744a87a6ac6371b7fe40bff7c017dcf036feabd
gitGudOrGitOut <47678302+gitGudOrGitOut@users.noreply.github.com>
2020-02-28 17:26:17 +0000

    Registering encrypted and password difficulty validation set

--- app/src/main/java/com/gangoffive/rig2gig/RegisterActivity.java
+++ app/src/main/java/com/gangoffive/rig2gig/RegisterActivity.java
@@ -22,10 +22,12 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.identityconnectors.common.security.GuardedString;
 import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * RegisterActivity loads when the activity is called.
@@ -58,10 +60,28 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void registerBtnOnClick(View view) {
+        AtomicBoolean validPass = new AtomicBoolean();
+        validPass.set(true);
+        AtomicBoolean matchingPass = new AtomicBoolean();
+        matchingPass.set(true);
         final String email = rEmailAddress.getText().toString().trim();
         String confirmEmail = rConfirmEmail.getText().toString().trim();
-        final String password = rPassword.getText().toString().trim();
-        String confirmPassword = rConfirmPassword.getText().toString().trim();
+
+        /*Creating an empty char array with the length of the password*/
+        char[] passChars = new char[rPassword.getText().length()];
+
+        /*Copying all the characters in the password textbox to the char array*/
+        rPassword.getText().getChars(0, rPassword.getText().length(), passChars, 0);
+
+        GuardedString encryptedPassword = new GuardedString(passChars);
+
+        /*Creating an empty char array with the length of the password*/
+        char[] confirmChars = new char[rConfirmPassword.getText().length()];
+
+        /*Copying all the characters in the password textbox to the char array*/
+        rConfirmPassword.getText().getChars(0, rConfirmPassword.getText().length(), confirmChars, 0);
+
+        GuardedString encryptedConfirm = new GuardedString(confirmChars);
 
         if (TextUtils.isEmpty(email)){
             rEmailAddress.setError("Email is required!");
@@ -75,61 +95,155 @@ public class RegisterActivity extends AppCompatActivity {
             rConfirmEmail.setError("Email doesn't match!");
             return;
         }
-        if (TextUtils.isEmpty(password)){
-            rPassword.setError("Password is required!");
-            return;
-        }
-        if (TextUtils.isEmpty(confirmPassword)){
-            rConfirmPassword.setError("Confirm password is required!");
-            return;
-        }
-        if (!confirmPassword.matches(password)){
-            rConfirmPassword.setError("Password doesn't match");
-            return;
-        }
-        if (password.length() < 6){
-            rPassword.setError("Password needs to be 6 characters or longer!");
-            return;
-        }
 
-        /**
-         * Creating an account with Firebase from the information that the user has inputted.
-         */
-        fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        /*Password validation check*/
+        /*Method used to access the encrypted password and get the clear chars*/
+        encryptedPassword.access(new GuardedString.Accessor() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    userId = fAuth.getUid();
-                    DocumentReference documentReference = fStore.collection("users").document(userId);
-                    Map<String, Object> user = new HashMap<>();
-                    user.put("Email Address", email);
-                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Toast.makeText(RegisterActivity.this, "Account has been created!", Toast.LENGTH_SHORT).show();
-                            Log.d(TAG, "onSuccess: user Profile is created for "+ userId);
-                            fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()){
-                                        startActivity(new Intent(getApplicationContext(),CredentialActivity.class));
-                                    }
-                                }
-                            });
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(RegisterActivity.this, "Error creating account", Toast.LENGTH_SHORT).show();
-                            Log.d(TAG, "onFailure: " + e.toString());
-                        }
-                    });
-
-                }else {
-                    Toast.makeText(RegisterActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+            public void access(char[] chars) {
+                char[] passChars = chars;
+                if(chars.length == 0)
+                {
+                    rPassword.setError("Password is required");
+                    validPass.set(false);
+                    return;
                 }
+                else if(chars.length < 8)
+                {
+                    rPassword.setError("Password needs to be 8 characters or longer!");
+                    validPass.set(false);
+                    return;
+                }
+                else
+                {
+                    boolean number = false, capital = false, space = false;
+                    for(char c : chars)
+                    {
+                        if(Character.isDigit(c))
+                        {
+                            number = true;
+                        }
+                        if(Character.isLetter(c) && Character.isUpperCase(c))
+                        {
+                            capital = true;
+                        }
+                        if(c == ' ')
+                        {
+                            space = true;
+                        }
+                    }
+                    if(!number)
+                    {
+                        rPassword.setError("Password needs at least a digit!");
+                        validPass.set(false);
+                        return;
+                    }
+                    if(!capital)
+                    {
+                        rPassword.setError("Password needs at least one upper-case letter!");
+                        validPass.set(false);
+                        return;
+                    }
+                    if(space)
+                    {
+                        rPassword.setError("No whitespaces allowed in the password!");
+                        validPass.set(false);
+                        return;
+                    }
+                }
+
+                /*Confirmation password check*/
+                /*Method used to access the encrypted password and get the clear chars*/
+                encryptedConfirm.access(new GuardedString.Accessor() {
+                    @Override
+                    public void access(char[] chars) {
+                        if(chars.length == 0)
+                        {
+                            rConfirmPassword.setError("Password confirmation must not be empty!");
+                            matchingPass.set(false);
+                            return;
+                        }
+                        else if(chars.length != passChars.length)
+                        {
+                            rConfirmPassword.setError("Passwords must be matching!");
+                            matchingPass.set(false);
+                            return;
+                        }
+                        for(int i = 0; i < chars.length; i++)
+                        {
+                            if(chars[i] != passChars[i])
+                            {
+                                rConfirmPassword.setError("Passwords must be matching!");
+                                matchingPass.set(false);
+                                return;
+                            }
+                        }
+                    }
+                });
             }
         });
+        if(!validPass.get())
+        {
+            return;
+        }
+        if(!matchingPass.get())
+        {
+            return;
+        }
+
+        /*Method used to access the encrypted password and get the clear chars*/
+        encryptedPassword.access(new GuardedString.Accessor() {
+            @Override
+            public void access(char[] chars) {
+                /**
+                 * Creating an account with Firebase from the information that the user has inputted.
+                 */
+                fAuth.createUserWithEmailAndPassword(email, new String(chars)).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            userId = fAuth.getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userId);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("Email Address", email);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(RegisterActivity.this, "Account has been created!", Toast.LENGTH_SHORT).show();
+                                    Log.d(TAG, "onSuccess: user Profile is created for "+ userId);
+                                    fAuth.signInWithEmailAndPassword(email, new String(passChars)).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()){
+                                                Log.d(TAG, "Opening credential activity");
+                                                startActivity(new Intent(getApplicationContext(),CredentialActivity.class));
+                                            }
+                                            else
+                                            {
+                                                Log.d(TAG, "Sign in failed: " + task.getException());
+                                            }
+                                        }
+                                    });
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(RegisterActivity.this, "Error creating account", Toast.LENGTH_SHORT).show();
+                                    Log.d(TAG, "onFailure: " + e.toString());
+                                }
+                            });
+
+                        }else {
+                            Toast.makeText(RegisterActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        /*Disposes of the encrypted password from the memory*/
+        encryptedConfirm.dispose();
+        encryptedPassword.dispose();
     }
 
     /**
