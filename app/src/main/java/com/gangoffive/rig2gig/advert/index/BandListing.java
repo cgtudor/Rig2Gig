ffef20d629e789a14d8ce9990fb7f6b8328c42c3
Paradisumal <55985977+Paradisumal@users.noreply.github.com>
2020-05-04 23:14:39 +0100

    Advert Index Method Comments

--- app/src/main/java/com/gangoffive/rig2gig/advert/index/BandListing.java
+++ app/src/main/java/com/gangoffive/rig2gig/advert/index/BandListing.java
@@ -9,15 +9,28 @@ public class BandListing {
 
     }
 
+    /**
+     *  Constructor to create a band advert object to hold required values to display advert elsewhere.
+     * @param listingRef the associated unique listing reference String for this advert.
+     * @param bandRef the unique band reference String associated to this advert.
+     */
     public BandListing(String listingRef, String bandRef) {
         this.listingRef = listingRef;
         this.bandRef = bandRef;
     }
 
+    /**
+     *  Accessor
+     * @return the associated unique listing reference String for this advert.
+     */
     public String getListingRef() {
         return listingRef;
     }
 
+    /**
+     *  Accessor
+     * @return the unique band reference String associated to this advert.
+     */
     public String getBandRef() {
         return bandRef;
     }
--- app/src/main/java/com/gangoffive/rig2gig/comms/Communication.java
+++ app/src/main/java/com/gangoffive/rig2gig/comms/Communication.java
@@ -18,6 +18,16 @@ public class Communication {
 
     }
 
+    /**
+     * Constructor to create a communication object to hold required values to display communication elsewhere.
+     * @param commRef the associated unique communication reference String for this advert.
+     * @param userRef the communication's recieving user's unique user reference.
+     * @param commType the associated communication type String for this advert.
+     * @param sentFromType the entity type String associated to the communication's sender.
+     * @param sentFromRef the unique entity reference String associated to the communication's sender.
+     * @param sentToType the entity type String associated to the communication's receiver.
+     * @param sentToRef the entity type String associated to the communication's receiver.
+     */
     public Communication(String commRef, String userRef, String commType, String sentFromType, String sentFromRef, String sentToType, String sentToRef) {
         this.commRef = commRef;
         this.userRef = userRef;
@@ -30,6 +40,17 @@ public class Communication {
         this.sentToRef = sentToRef;
     }
 
+    /**
+     * Constructor to create a communication object to hold required values to display communication elsewhere.
+     * @param commRef the associated unique communication reference String for this advert.
+     * @param userRef the communication's recieving user's unique user reference.
+     * @param commType the associated communication type String for this advert.
+     * @param sentFromType the entity type String associated to the communication's sender.
+     * @param sentFromRef the unique entity reference String associated to the communication's sender.
+     * @param sentToType the entity type String associated to the communication's receiver.
+     * @param sentToRef the entity type String associated to the communication's receiver.
+     * @param musicianRef the unique musician reference String associated to the communication.
+     */
     public Communication(String commRef, String userRef, String commType, String sentFromType, String sentFromRef, String sentToType, String sentToRef, String musicianRef) {
         this.commRef = commRef;
         this.userRef = userRef;
@@ -44,24 +65,60 @@ public class Communication {
         this.musicianRef = musicianRef;
     }
 
+    /**
+     *  Accessor
+     * @return the associated unique communication reference String for this advert.
+     */
     public String getCommRef() {
         return commRef;
     }
 
+    /**
+     *  Accessor
+     * @return the communication's recieving user's unique user reference.
+     */
     public String getUserRef() { return userRef; }
 
+    /**
+     *  Accessor
+     * @return the communication type String for this advert.
+     */
     public String getCommType() { return commType; }
 
+    /**
+     *  Accessor
+     * @return the entity type String associated to the communication's sender.
+     */
     public String getSentFromType() { return sentFromType; }
 
+    /**
+     *  Accessor
+     * @return the unique entity reference String associated to the communication's sender.
+     */
     public String getSentFromRef() { return sentFromRef; }
 
+    /**
+     *  Accessor
+     * @return the entity type String associated to the communication's receiver.
+     */
     public String getSentToType() { return sentToType; }
 
+    /**
+     *  Accessor
+     * @return the unique entity reference String associated to the communication's receiver.
+     */
     public String getSentToRef() { return sentToRef; }
 
+    /**
+     *  Accessor
+     * @return the unique musician reference String associated to the communication.
+     */
     public String getMusicianRef() { return musicianRef; }
 
+    /**
+     *  Mutator
+     * @param commType the communication type String for this advert.
+     */
     public void setCommType(String commType) {
         this.commType = commType;
     }
